527d973018e979ca6c161d77dec450b3dcfaac8e
SAURABH KUMAR <147742073+skkushwah03@users.noreply.github.com>
2025-10-27 14:53:54 +0530

    Create L1.java

---
+++ L1.java
@@ -0,0 +1,25 @@
+package _8_LinkedList;
+
+public class L1 {
+    class Node{
+     int data;   
+     Node next;
+   
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+    public static Node head;
+    public static Node tail;
+
+    //method
+    
+
+    public static void main(String[] args) {
+        L1 lst=new L1();
+        // lst.head=new Node(1);
+        // lst.head.next=new Node(2);
+
+    }
+}
